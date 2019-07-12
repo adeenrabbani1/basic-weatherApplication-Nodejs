e748a7eeccca4fb348743b8c265b6ca6c1bdd019
adeen <adeen.rabbani@hotmail.com>
2019-07-12 14:55:22 +0800

    setup for Heroku.

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ weatherForm.addEventListener('submit', (e)=>{
     heading.textContent = ''
     weather.textContent = "Loading..."
 
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
 
      response.json().then((data)=>{
          if(data.Error){
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const forecast = require('./utils/forecast')
 const express = require ('express') 
 const hbs = require('hbs') //setting up handlebars to render templates!
  const app = express()
+ const port = process.env.PORT
 //setting up express configurations
   const publicPath = path.join(__dirname , '../public')
   const viewPath = path.join(__dirname , '../templates/views' )
@@ -100,7 +101,7 @@ app.get('/' , (req, res) =>{
      })
  })
 
- app.listen(3000,()=>{
+ app.listen(port,()=>{
      console.log('server is up and running!');
      
  })
